678693291743ec5b031bb1d1366206a37a32518d
macdja38 <jakeincanada@icloud.com>
2017-02-02 23:33:07 -0500

    add db connect config, require auth for connecting.

--- server.js
+++ server.js
@@ -8,11 +8,24 @@ const WebSocket = require('ws');
 
 const OpCodes = require('./OpCodes');
 
-const r = require('rethinkdbdash')({
-  port: '28015',
-  host: 'localhost',
-  db: 'tau'
-});
+const auth = require('./config/auth.json');
+
+const r = require('rethinkdbdash')(auth.rethinkdb);
+
+function ensure(r, tableName) {
+  r.tableList().contains(tableName)
+    .do(function(databaseExists) {
+      return r.branch(
+        databaseExists,
+        { table_created: 0 },
+        r.tableCreate(tableName)
+      );
+    }).run();
+}
+
+ensure(r, "settingsBot");
+ensure(r, "settingsMap");
+ensure(r, "settings");
 
 const app = express();
 
@@ -25,22 +38,27 @@ let clientId = "38383838338";
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-  console.log('connection?');
-  const location = url.parse(ws.upgradeReq.url, true);
-  console.log('location', location);
-  // You might use location.query.access_token to authenticate or share sessions
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+class Client {
+  constructor(ws, id) {
+    this.ws = ws;
+    this.incomingMessage = this.incomingMessage.bind(this);
+    this.send = this.send.bind(this);
+    this.send({op: OpCodes.HELLO,
+      d: {
+      heartbeat_interval: 15000
+    }});
+    ws.on("message", this.incomingMessage);
+  }
 
-  ws.on('message', function incoming(message) {
+  incomingMessage(message) {
     let contents = JSON.parse(message);
     console.log('received ', contents, message);
     switch (contents.op) {
       case OpCodes.IDENTIFY:
-        ws.send(JSON.stringify({
+        this.send({
           op: OpCodes.DISPATCH,
           t: "READY",
-        }));
+        });
         break;
       case OpCodes.REQUEST_GUILD:
         console.log(JSON.stringify(contents.d.guilds.map(g => `${clientId}|${g}`)));
@@ -56,25 +74,41 @@ wss.on('connection', function connection(ws) {
               console.log(value.new_val);
               const parsedData = value.new_val;
               parsedData.id = parsedData.id.split("|")[1];
-              ws.send(JSON.stringify({
+              this.send({
                 op: OpCodes.DISPATCH,
                 t: "GUILD_CONFIG_UPDATE",
                 d: parsedData ,
-              }))
+              });
             })
           });
         break;
     }
-  });
+  }
 
-  ws.send(JSON.stringify({
-    op: OpCodes.HELLO,
-    d: {
-      heartbeat_interval: 15000
-    }
-  }));
-});
+  send(object) {
+    this.ws.send(JSON.stringify(object));
+  }
+}
 
-server.listen(8080, function listening() {
+wss.on('connection', handleConnection);
+
+function handleConnection(ws) {
+  let headers = ws.upgradeReq.headers;
+  console.log('connection?', headers);
+  if (headers.token && headers.id) {
+    r.table("settingsBot").get(headers.id).then((settings) => {
+      if (settings !== null) {
+        if (settings.token === headers.token) {
+          return new Client(ws, headers.id);
+        }
+      }
+      ws.terminate(403);
+    })
+  } else {
+    ws.terminate(403)
+  }
+}
+
+server.listen(8089, function listening() {
   console.log('Listening on %d', server.address().port);
 });
