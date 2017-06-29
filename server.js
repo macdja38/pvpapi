4b067787c1389e47daf9b93cfd381d348ac5ed48
macdja38 <jakeincanada@icloud.com>
2017-06-29 06:24:04 -0400

    Add the ability to fetch guild info from pvpClient

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const auth = require('./config/auth.json');
 
 const r = require('rethinkdbdash')(auth.rethinkdb);
 
+let clients = [];
+
 function ensure(r, tableName) {
   r.tableList().contains(tableName)
-    .do(function(databaseExists) {
+    .do(function (databaseExists) {
       return r.branch(
         databaseExists,
-        { table_created: 0 },
+        {table_created: 0},
         r.tableCreate(tableName)
       );
     }).run();
@@ -29,12 +31,49 @@ ensure(r, "settings");
 
 const app = express();
 
+let currentRequets = {};
+
+app.get('/v1/server/:id/', (req, res) => {
+  let clientID = req.headers.id;
+  if (!clientID) {
+    res.status(400).send("id header not present")
+  }
+  console.log(req.headers, req.params.id);
+  r.table("settingsBot").get(clientID).run().then((settings) => {
+    if (settings === null || settings.token !== req.headers.token) {
+      return res.rejectUnauthorized();
+    }
+    let possibleClients = clients.filter(c => c.id === clientID).filter(c => c.guildSet && c.guildSet.has(clientID + "|" + req.params.id));
+    if (possibleClients.length > 0) {
+      console.log(possibleClients);
+      let client = possibleClients[0];
+      let nonce = Math.random()*100000;
+      client.send({
+        op: OpCodes.GET_CHANNELS_USERS_AND_ROLES,
+        nonce: nonce,
+        d: {id: req.params.id}
+      });
+      (new Promise((resolve, reject) => {
+        currentRequets[nonce] = {resolve, reject}
+      })).then(data => res.json({data})).catch((error) => res.status(500).send(error.toString()));
+      setTimeout(() => {
+        if (currentRequets.hasOwnProperty(nonce)) {
+          currentRequets[nonce].reject("Timed out");
+          delete currentRequets[nonce];
+        }
+      }, 5000)
+    } else {
+      res.status(404).send("guild with that id was not currently available");
+    }
+  });
+});
+
 app.use(function (req, res) {
-  res.send({ msg: "hello" });
+  res.send({msg: "hello"});
 });
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({server});
 
 class Client {
   constructor(ws, id) {
@@ -43,10 +82,12 @@ class Client {
     this.guildSet = false;
     this.incomingMessage = this.incomingMessage.bind(this);
     this.send = this.send.bind(this);
-    this.send({op: OpCodes.HELLO,
+    this.send({
+      op: OpCodes.HELLO,
       d: {
-      heartbeat_interval: 15000
-    }});
+        heartbeat_interval: 15000
+      }
+    });
     ws.on("message", this.incomingMessage);
   }
 
@@ -72,17 +113,17 @@ class Client {
         }
         let settingsTable = r.table('settings');
         settingsTable.getAll(...newGuildList).run().then((values) => {
-            values.forEach((value) => {
-                value.id = value.id.split("|")[1];
-                this.send({
-                    op: OpCodes.DISPATCH,
-                    t: "GUILD_CONFIG_UPDATE",
-                    d: value ,
-                });
-            })
+          values.forEach((value) => {
+            value.id = value.id.split("|")[1];
+            this.send({
+              op: OpCodes.DISPATCH,
+              t: "GUILD_CONFIG_UPDATE",
+              d: value,
+            });
+          })
         });
         settingsTable
-          .getAll(...this.guildSet, { index: 'id' })
+          .getAll(...this.guildSet, {index: 'id'})
           .changes({
             squash: true,
           })
@@ -105,7 +146,7 @@ class Client {
               this.send({
                 op: OpCodes.DISPATCH,
                 t: "GUILD_CONFIG_UPDATE",
-                d: parsedData ,
+                d: parsedData,
               });
             })
           });
@@ -114,11 +155,17 @@ class Client {
         contents.d.data.id = `${this.id}|${contents.d.id}`;
         r.table('settingsMap').insert(contents.d.data, {conflict: contents.d.o}).run();
         break;
+      case OpCodes.RESPONSE_CHANNELS_USERS_AND_ROLES:
+        console.log(contents);
+        if (currentRequets.hasOwnProperty(contents.nonce)) {
+          currentRequets[contents.nonce].resolve(contents.d);
+          delete currentRequets[contents.nonce];
+        }
     }
   }
 
   send(object) {
-    this.ws.send(JSON.stringify(object));
+    return this.ws.send(JSON.stringify(object));
   }
 }
 
@@ -131,7 +178,9 @@ function handleConnection(ws) {
     r.table("settingsBot").get(headers.id).then((settings) => {
       if (settings !== null) {
         if (settings.token === headers.token) {
-          return new Client(ws, headers.id);
+          let client = new Client(ws, headers.id);
+          clients.push(client);
+          return client;
         }
       }
       ws.terminate(403);
