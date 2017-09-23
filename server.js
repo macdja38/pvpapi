a7ccb05beaf2de6fa0cd2fbe1349490a8568f64b
macdja38 <jakeincanada@icloud.com>
2017-09-23 16:35:34 -0400

    Allow a guild being removed from the bot to remove it from the api so that the website will properly display an invite link to invite the bot to the discord.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function ensure(r, tableName) {
       return r.branch(
         databaseExists,
         {table_created: 0},
-        r.tableCreate(tableName)
+        r.tableCreate(tableName),
       );
     }).run();
 }
@@ -45,11 +45,11 @@ app.get('/v1/server/:id/', (req, res) => {
     let possibleClients = clients.filter(c => c.id === clientID).filter(c => c.guildSet && c.guildSet.has(clientID + "|" + req.params.id));
     if (possibleClients.length > 0) {
       let client = possibleClients[0];
-      let nonce = Math.random()*100000;
+      let nonce = Math.random() * 100000;
       client.send({
         op: OpCodes.GET_CHANNELS_USERS_AND_ROLES,
         nonce: nonce,
-        d: {id: req.params.id}
+        d: {id: req.params.id},
       });
       (new Promise((resolve, reject) => {
         currentRequets[nonce] = {resolve, reject}
@@ -84,8 +84,8 @@ class Client {
     this.send({
       op: OpCodes.HELLO,
       d: {
-        heartbeat_interval: 15000
-      }
+        heartbeat_interval: 15000,
+      },
     });
     ws.on("message", this.incomingMessage);
     ws.once("close", this.onClose);
@@ -100,20 +100,25 @@ class Client {
   incomingMessage(message) {
     let contents = JSON.parse(message);
     switch (contents.op) {
-      case OpCodes.IDENTIFY:
+      case OpCodes.IDENTIFY: {
         this.send({
           op: OpCodes.DISPATCH,
           t: "READY",
         });
         break;
-      case OpCodes.REQUEST_GUILD:
-        let newGuildList = contents.d.guilds.map(g => `${this.id}|${g}`);
+      }
+      case OpCodes.REMOVE_GUILD: {
+        const removeGuildList = contents.d.guilds.map(g => `${this.id}|${g}`);
+        if (!this.guildSet) return;
+        removeGuildList.forEach(guildIdentifier => this.guildSet.delete(guildIdentifier));
+        break;
+      }
+      case OpCodes.REQUEST_GUILD: {
+        const newGuildList = contents.d.guilds.map(g => `${this.id}|${g}`);
         if (!this.guildSet) {
-          this.guildSet = new Set(newGuildList)
+          this.guildSet = new Set(newGuildList);
         } else {
-          newGuildList.forEach(guildIdentifier => {
-            this.guildSet.add(guildIdentifier);
-          })
+          newGuildList.forEach(guildIdentifier => this.guildSet.add(guildIdentifier));
         }
         let settingsTable = r.table('settings');
         settingsTable.getAll(...newGuildList).run().then((values) => {
@@ -155,15 +160,18 @@ class Client {
             })
           });
         break;
-      case OpCodes.UPDATE_CONFIG:
+      }
+      case OpCodes.UPDATE_CONFIG: {
         contents.d.data.id = `${this.id}|${contents.d.id}`;
         r.table('settingsMap').insert(contents.d.data, {conflict: contents.d.o}).run();
         break;
-      case OpCodes.RESPONSE_CHANNELS_USERS_AND_ROLES:
+      }
+      case OpCodes.RESPONSE_CHANNELS_USERS_AND_ROLES: {
         if (currentRequets.hasOwnProperty(contents.nonce)) {
           currentRequets[contents.nonce].resolve(contents.d);
           delete currentRequets[contents.nonce];
         }
+      }
     }
   }
 
